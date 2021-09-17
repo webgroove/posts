053d2db49ae96478f22d909b5fca4f08faee799e
Takuya Ihara <webgroove1@gmail.com>
2021-09-17 16:03:37 +0900

    add : PHP でコメントを素早くスイッチする方法
    https://webgroove.work/php-comment-quick-switch/

---
+++ post3912/index.php
@@ -0,0 +1,24 @@
+<?php
+const API_KEY = /**/ '1yKWejFX' /*/ 'uVd8Wj0T' /**/;
+
+var_dump(API_KEY);
+
+/**/
+const API_URL_2 = '1yKWejFX';
+const API_KEY_2 = 'https://webgroove.work/';
+/*/
+const API_URL_2 = 'uVd8Wj0T';
+const API_KEY_2 = 'https://dev.webgroove.work/';
+/**/
+
+var_dump(API_URL_2, API_KEY_2);
+
+//*
+const API_URL_3 = '1yKWejFX';
+const API_KEY_3 = 'https://webgroove.work/';
+/*/
+const API_URL_3 = 'uVd8Wj0T';
+const API_KEY_3 = 'https://dev.webgroove.work/';
+//*/
+
+var_dump(API_URL_3, API_KEY_3);
